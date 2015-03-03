4a0a558c2b49df34a26cdd32bd8a48cdf3f35ae6
Administrator <Administrator@ADMINISTRADOR-H.diagnostico.com>
2015-03-03 16:08:52 -0600

    HAciendo el primer add usando github con netbeans

---
+++ Pruebagit/src/pruebagit/Pruebagit.java
@@ -0,0 +1,17 @@
+
+package pruebagit;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Pruebagit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
